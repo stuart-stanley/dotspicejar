ae6876c42192045410671264e1708449aaa7df64
Stuart Stanley <stu00@mac.com>
2019-09-15 20:44:21 -0700

    add "raised" method and fix cell-states init

--- src/braille/braille_cell.py
+++ src/braille/braille_cell.py
@@ -1,3 +1,4 @@
+import copy
 
 
 class BrailleCell(object):
@@ -9,7 +10,11 @@ class BrailleCell(object):
             "invalid column 1, incorrect length: ''".format(dot_col_1)
         assert len(dot_col_2) == 3, \
             "invalid column 2, incorrect length: ''".format(dot_col_2)
-        self.__dots = [[False, False]] * 3
+        self.__dots = [
+            [False, False],
+            [False, False],
+            [False, False]
+        ]
         left_bits = self.__build_row(0, dot_col_1, 1)
         right_bits = self.__build_row(1, dot_col_2, 8)
         ucode_value = chr(self.BRAILLE_UNICODE_BASE + right_bits + left_bits)
@@ -22,6 +27,13 @@ class BrailleCell(object):
     @property
     def unicode(self):
         return self.__unicode
+
+    @property
+    def key(self):
+        return self.__key
+
+    def raised(self, over, down):
+        return self.__dots[down][over]
 
     def __build_row(self, row_number, row_text, shift_base):
         bits = 0
